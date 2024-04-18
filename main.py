72c8076d490e10e9b0ea543370dfddd76bebce3b
FourZd <shogo.ryaskov@gmail.com>
2024-04-18 12:49:47 +0300

    fix routers

--- main.py
+++ main.py
@@ -3,4 +3,5 @@ from routers.auth import router as auth_router
 from routers.casino import router as casino_router
 app = FastAPI()
 
-app.include_router(auth_router, casino_router)
+app.include_router(auth_router)
+app.include_router(casino_router)
