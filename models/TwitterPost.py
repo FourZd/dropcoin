13284bbfc5756b9ae761c3946620f4131174223b
FourZd <shogo.ryaskov@gmail.com>
2024-04-24 01:06:46 +0300

    alter twitter id

--- models/TwitterPost.py
+++ models/TwitterPost.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import relationship
 class TwitterPost(EntityMeta):
     __tablename__ = "twitter_posts"
 
-    id = Column(String, primary_key=True, index=True)
+    id = Column(Integer, primary_key=True, autoincrement=True, index=True)
     user_id = Column(String, ForeignKey('users.id'))
     created_at = Column(DateTime(timezone=True))
     post_type = Column(String)
